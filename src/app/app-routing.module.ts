9a82cc5af722b65cd6a3e8882110bfc6d13a733a
Adam Owen <ahowenjr@alaska.edu>
2023-01-23 14:05:22 -0900

    routting fixed. nextt step add all 12 months

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { JanuaryComponent } from './month/january/january.component';
 import { FebuaryComponent } from './month/febuary/febuary.component';
 import { MarchComponent } from './month/march/march.component';
+import { MonthComponent } from './month/month.component';
 
 const routes: Routes = [
+  {path: '', component: MonthComponent},
   {path: 'month/january',component: JanuaryComponent },
   {path: 'month/febuary', component: FebuaryComponent },
   {path: 'month/march', component: MarchComponent },
