ce8b87a36733c8446ef5bdeb482d2289c0fcb24d
Adam Owen <ahowenjr@alaska.edu>
2023-02-07 15:23:02 -0900

    testing endpoint functitons

--- frontend/my-app/src/app/month/01_january/january.component.ts
+++ frontend/my-app/src/app/month/01_january/january.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { catchError, of, switchMap, tap } from 'rxjs'
+import { NotesService } from 'src/app/notes.service';
 
 @Component({
   selector: 'app-january',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./january.component.css']
 })
 export class JanuaryComponent {
+  title = 'frontend';
 
+  constructor(noteService: NotesService){
+
+    noteService.getNotes().pipe(  // Get all notes
+      tap(notes => console.log(notes)),
+      switchMap(notes => {
+        const note = { ...notes[0] }
+        note.comments += ' ADDED NOTES'
+        return noteService.updateNote(note)
+      }),
+      catchError(() => {
+        console.warn('Could not update user that does not exist')
+        return of([])
+      }),
+      switchMap(() => noteService.getNotes()), // Get all notes
+      tap(notes => console.log(notes)),
+      switchMap(notes => noteService.deleteNote(notes[0])), // Delete first note
+      catchError(() => {
+        console.warn('Could not delete user that does not exist')
+        return of([])
+      }),
+      switchMap(() => noteService.getNotes()), // Get all notes
+      tap(users => console.log(users)),
+    ).subscribe()
+    
+  }
 }
